f15107e6bffb8ac287a40fd34798201cb7f946c6
Matthew Denwood <md@sund.ku.dk>
2019-08-06 13:20:15 +0200

    Add binary TestRcpp

--- using.R
+++ using.R
@@ -3,4 +3,14 @@
 library('drat')
 dir <- '~/Documents/GitHub/drat'
 
+# R CMD build TestRcpp
+# R CMD install --build TestRcpp_0.1-1.tar.gz
+# And win-builder
 insertPackage('TestRcpp_0.1-1.tar.gz', dir)
+insertPackage('TestRcpp_0.1-1.tgz', dir)
+# Then push
+
+# Then:
+addRepo('ku-awdc')
+getOption("repos")
+install.packages('TestRcpp')
